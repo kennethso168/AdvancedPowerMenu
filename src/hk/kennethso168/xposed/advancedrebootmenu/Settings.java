0af999f5f879967408ccfb1a1a149aed3374c9ca
kennethso168 <kennethso168@gmail.com>
2013-09-15 00:38:27 +0800

    Better settings descriptions
    - Change settings summary so that it follows android design guidelines
    - "Confirmation Dialogs" option summary now show the current selection
    instead of a description
    - The app info is more "reusable" (easier to translate) now

--- src/hk/kennethso168/xposed/advancedrebootmenu/Settings.java
+++ src/hk/kennethso168/xposed/advancedrebootmenu/Settings.java
@@ -1,24 +1,56 @@
 package hk.kennethso168.xposed.advancedrebootmenu;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import de.robv.android.xposed.XposedBridge;
 
 public class Settings extends Activity {
+	private final String CLASS = "Settings.java";
+	private void log(String message) {
+    	if(Main.writeLogs()) XposedBridge.log(CLASS + ": " + message);
+    }
+    
+    private void log(Throwable t){
+    	if(Main.writeLogs()){
+    		XposedBridge.log(CLASS + ": ");
+    		XposedBridge.log(t);
+    	}    	
+    }
+	
+	private static String versionName;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		setTitle(R.string.app_name);
 		super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
+        
+		// Display the fragment as the main content.
         if (savedInstanceState == null)
 			getFragmentManager().beginTransaction().replace(android.R.id.content,
 	                new PrefsFragment()).commit();
+        try {
+        	PackageManager manager = this.getPackageManager();
+			PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+			versionName = info.versionName;
+			
+		} catch (NameNotFoundException e) {
+			log(e);
+		}
+        
 	}
 
-
-
-	public static class PrefsFragment extends PreferenceFragment {
+	public static class PrefsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener{
+		public static final String KEY_PREF_CONFIRM_DIALOG = "pref_confirm_dialog";
+		public static final String KEY_PREF_APP_INFO = "pref_app_info";
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
@@ -27,6 +59,41 @@ public class Settings extends Activity {
 			// are in the same package, they are executed in the context of the hooked package
 			getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
 			addPreferencesFromResource(R.xml.preferences);
+			int prefConfirmDialogValue = Integer.parseInt(getPreferenceScreen().getSharedPreferences().getString(KEY_PREF_CONFIRM_DIALOG, "0"));
+			Resources res = getResources();
+			String[] prefConfirmDialogDesc = res.getStringArray(R.array.confirm_dialog);
+			findPreference(KEY_PREF_CONFIRM_DIALOG).setSummary(prefConfirmDialogDesc[prefConfirmDialogValue]);
+			String aboutBefore = getResources().getString(R.string.app_info_before);
+			String aboutAfter = getResources().getString(R.string.app_info_after);
+			findPreference(KEY_PREF_APP_INFO).setSummary(aboutBefore + versionName + aboutAfter);
+			
+		}
+		
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+			if (key.equals(KEY_PREF_CONFIRM_DIALOG))
+	        {
+	            // Set summary to be the user-description for the selected value
+	            Preference pref = findPreference(key);
+	            int prefConfirmDialogValue = Integer.parseInt(sharedPreferences.getString(KEY_PREF_CONFIRM_DIALOG, "0"));
+	            Resources res = getResources();
+				String[] prefConfirmDialogDesc = res.getStringArray(R.array.confirm_dialog);
+	            pref.setSummary(prefConfirmDialogDesc[prefConfirmDialogValue]);
+	        }
+	    }
+		
+		@Override
+		public void onResume() {
+		    super.onResume();
+		    getPreferenceScreen().getSharedPreferences()
+		            .registerOnSharedPreferenceChangeListener(this);
+		}
+
+		@Override
+		public void onPause() {
+		    super.onPause();
+		    getPreferenceScreen().getSharedPreferences()
+		            .unregisterOnSharedPreferenceChangeListener(this);
 		}
 	}
 }
