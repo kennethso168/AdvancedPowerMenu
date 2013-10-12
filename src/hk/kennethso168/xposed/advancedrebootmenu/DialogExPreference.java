04dfb2345ee58930da6fdd4120cd8ed89a5202fc
kennethso168 <kennethso168@gmail.com>
2013-10-12 23:37:34 +0800

    Add readme, remove link to license - Add a readme - Removed the confusing link to the open source license

---
+++ src/hk/kennethso168/xposed/advancedrebootmenu/DialogExPreference.java
@@ -0,0 +1,12 @@
+package hk.kennethso168.xposed.advancedrebootmenu;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class DialogExPreference extends DialogPreference{
+	public DialogExPreference(Context oContext, AttributeSet attrs)
+    {
+        super(oContext, attrs);     
+    }
+}
